c24ec510fcee5f18afc930649a0f0ab665d3523a
lyna <vuthilyna304@gmail.com>
2020-04-07 16:44:28 +0700

    fix mirgations

--- database/migrations/20200402114419_merchants_products.js
+++ database/migrations/20200402114419_merchants_products.js
@@ -1,13 +1,12 @@
-
 exports.up = (knex) => {
-  return knex.schema.createTable('Product_Type', (table) => {
+  return knex.schema.createTable('product_type', (table) => {
     table.increments('id').primary();
     table.string('product_type_name', 255).notNullable();
-    table.integer('user_id').unsigned()
-                            .references('id')
-                            .inTable('users');
+    table.foreign('user_id').unsigned()
+      .references('id')
+      .inTable('users');
   });
 };
 exports.down = (knex) => {
-  return knex.schema.dropTable('Product_type');
+  return knex.schema.dropTable('product_type');
 };
--- database/migrations/20200402154305_products.js
+++ database/migrations/20200402154305_products.js
@@ -1,13 +1,14 @@
 
 exports.up = (knex) => {
-  return knex.schema.createTable('Products', (table) => {
+  return knex.schema.createTable('products', (table) => {
     table.increments('id').primary();
     table.string('product_name', 255).notNullable();
     table.string('describe',255).notNullable();
     table.integer('price').notNullable();
-    table.integer('product_type_id').unsigned()
+    table.foreign('product_type_id').unsigned()
                             .references('id')
-                            .inTable('Product_type');
+                            .inTable('product_type');
+                          
   });
 };
 
